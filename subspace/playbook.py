2a135d22b3375fc1a05be0fe6d1bcd3908c41b88
J. Matt Peterson <jmatt@jmatt.org>
2015-04-28 17:13:17 -0700

    Add get_playbooks method.     * Supports recursive directories of playbooks
    Add comments to factory and get_playbooks.
        * Add CHANGES file.
        * Update Ansible version in requirements.txt.
    
    	new file:   CHANGES
    	modified:   requirements.txt
    	modified:   subspace/playbook.py
    	modified:   subspace/version.py

--- subspace/playbook.py
+++ subspace/playbook.py
@@ -1,6 +1,8 @@
 """
 """
 import collections
+import operator
+import os
 
 import ansible.playbook
 from ansible import callbacks as ansible_callbacks
@@ -8,12 +10,16 @@ from ansible import utils
 
 from . import logging
 
+
 class PlayBook(object):
+
     @classmethod
     def factory(cls, playbook, inventory=None, stats=None, callbacks=None,
-                runner_callbacks=None, check=True, host_list=None, limit=None,
-                extra_vars=None, skip_tags=None, only_tags=["all"], logger=None):
+                runner_callbacks=None, check=True, host_list=None,
+                limit=None, extra_vars=None, skip_tags=None,
+                only_tags=["all"], logger=None):
         """
+        Create and return an ansible,playbook.PlayBook instance.
         :inventory: Ansible Inventory instance to setup.
         :limit: A dictionary or list of dictionaries limiting the PlayBook.
             Example:
@@ -25,6 +31,10 @@ class PlayBook(object):
                      passed as extra variables to the playbook.
         :only_tags: A list of tags to run in the playbook. Defaults to all.
         :skip_tags: A list of tags to skip in the playbook.
+
+        Notes: PlayBook is created, but not run. Generally if something isn't
+        passed to the factory it'll use similar behavior to the
+        ansible-playbook command-line script.
         """
         if logger:
             logging.use_logger(logger)
@@ -43,7 +53,7 @@ class PlayBook(object):
 
     @classmethod
     def _setup(cls, playbook, inventory, stats, callbacks, runner_callbacks,
-              host_list, limit):
+               host_list, limit):
         """
         Setup dependencies for the Ansible PlayBook and return them
         in a namedtuple.
@@ -84,7 +94,7 @@ class PlayBook(object):
             Example:
             {"hostname": "server", "ip": "1.2.3.4"}
             or
-            [{"hostname": "server1", "ip": "1.2.3.4"}, 
+            [{"hostname": "server1", "ip": "1.2.3.4"},
              {"hostname": "server2", "ip": "1.2.3.5"}]
         """
         if isinstance(limit, dict):
@@ -109,3 +119,34 @@ class PlayBook(object):
             raise Exception("The host (%s) is not in the "
                             "Ansible inventory file %s." %
                             (host, inventory.host_list))
+
+
+def _get_files(directory):
+    files = []
+    directories = list(os.walk(directory))
+    directories.sort(cmp=operator.lt)
+    for d in directories:
+        a_dir = d[0]
+        files_in_dir = d[2]
+        files_in_dir.sort()
+        if os.path.isdir(a_dir) and "playbooks" in a_dir:
+            for f in files_in_dir:
+                if os.path.splitext(f)[1] == ".yml":
+                    files.append(os.path.join(a_dir, f))
+    return files
+
+
+def get_playbooks(directory, **kwargs):
+    """
+    Walk the Directory structure and return an ordered list of
+    playbook objects.
+
+    :directory: The directory to walk and search for playbooks.
+    :kwargs: The keywords that will be passed to the PlayBook.factory method.
+
+    Notes: * Playbook files are identified as ending in .yml
+           * Playbooks are created using the same **kwargs.
+           * Playbooks are not run.
+           * Playbook files use the .yml file extension.
+    """
+    return [PlayBook.factory(pb, **kwargs) for pb in _get_files(directory)]

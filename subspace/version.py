a5549cbab402e5d641f0779878148210c26b846f
J. Matt Peterson <jmatt@jmatt.org>
2015-07-02 14:08:51 -0700

    Increment version to 0.1.5.
    	modified:   subspace/version.py

--- subspace/version.py
+++ subspace/version.py
@@ -5,7 +5,7 @@ from subprocess import Popen, PIPE
 from os.path import abspath, dirname
 
 
-VERSION = (0, 1, 4, 'dev', 0)
+VERSION = (0, 1, 5, 'dev', 0)
 
 
 git_match = "(?P<git_flag>git://)\S*#egg="\

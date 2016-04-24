aa59f4c1953e8b521c82d03e7cbdb59e76fc60d3
Steve Gregory <contact@steve-gregory.com>
2016-04-24 14:56:01 -0700

    Refactor 'playbook_2' to 'runner.py'

--- subspace/logging.py
+++
@@ -1,6 +0,0 @@
-# Replace with https://serversforhackers.com/running-ansible-2-programmatically
-# IF necessary.
-# For now, use ansible.cfg and:
-# [defaults]
-# log_path = /var/log/ansible/ansible.log
-#
--- subspace/runner.py
+++ subspace/runner.py
@@ -12,9 +12,9 @@ from ansible.utils.display import Display
 default_logger = logging.getLogger(__name__)
 
 
-class Options(object):
+class RunnerOptions(object):
     """
-    Options class to replace Ansible OptParser
+    RunnerOptions class to replace Ansible OptParser
     """
     def __init__(self, verbosity=None, inventory=None, listhosts=None, subset=None, module_paths=None, extra_vars=None,
                  forks=None, ask_vault_pass=None, vault_password_files=None, new_vault_password_file=None,
@@ -75,12 +75,19 @@ class Options(object):
 
 class Runner(object):
 
+    inventory = None
+    loader = None
+    options = None
+    playbooks = None
+    run_data = None
+    variable_manager = None
+
     def __init__(self, hosts_file, playbook, private_key_file, run_data,
                  limit_hosts=None, group_vars_map={}, become_pass=None, verbosity=0, logger=None):
 
         self.run_data = run_data
 
-        self.options = Options(
+        self.options = RunnerOptions(
                 verbosity=verbosity,
                 private_key_file=private_key_file,
                 subset=limit_hosts,
@@ -90,6 +97,51 @@ class Runner(object):
                 become_method='sudo',
                 become_user='root',
             )
+
+        self._set_verbosity()
+        self._set_loader()
+        self._set_variable_manager()
+        self._set_inventory(hosts_file)
+        self._set_playbooks(playbook)
+        self._set_hosts(group_vars_map)
+
+        # Become Pass Needed if not logging in as user root
+        # passwords = {'become_pass': become_pass}
+
+        # Setup playbook executor, but don't run until run() called
+        self.pbex = playbook_executor.PlaybookExecutor(
+            playbooks=self.playbooks,
+            inventory=self.inventory,
+            variable_manager=self.variable_manager,
+            loader=self.loader,
+            options=self.options,
+            passwords=None)
+
+    def _include_group_vars(self, group_vars_map={}):
+        variables = self.inventory.get_vars(self.host_limit)
+        group_names = variables.get('group_names',[])
+        for group_name in group_names:
+            file_path = group_vars_map.get(group_name,'')
+            if os.path.exists(file_path):
+                self.variable_manager.add_group_vars_file(file_path, self.loader)
+        self.options.logger.info("Vars found for hostname %s: %s" % (self.host_limit, variables))
+
+    def _set_playbooks_from_path(self, playbook_path):
+        # Convert file path to list of playbooks:
+        if not os.path.exists(playbook_path):
+            raise ValueError("Could not find path: %s" % (playbook_path,))
+
+        if os.path.isdir(playbook_path):
+            playbook_list = get_playbook_files(playbook_path)
+        else:
+            playbook_list = [playbook_path]
+
+        self.playbooks = playbook_list
+
+    def _set_verbosity(self):
+        """
+        FIXME: Prove that this works.
+        """
         # Set global verbosity
         self.display = Display()
         self.display.verbosity = self.options.verbosity
@@ -97,73 +149,60 @@ class Runner(object):
         # verbosity object/setting as well
         playbook_executor.verbosity = self.options.verbosity
 
-        # Become Pass Needed if not logging in as user root
-        # passwords = {'become_pass': become_pass}
-
+    def _set_loader(self):
         # Gets data from YAML/JSON files
         self.loader = DataLoader()
         if 'VAULT_PASS' in os.environ:
             self.loader.set_vault_password(os.environ['VAULT_PASS'])
 
+    def _set_variable_manager(self):
         # All the variables from all the various places
         self.variable_manager = VariableManager()
         self.variable_manager.extra_vars = self.run_data
 
+    def _set_inventory(self, hosts_file):
         if not os.path.exists(hosts_file):
             raise Exception("Could not find hosts file: %s" % hosts_file)
-
 
         # Set inventory, using most of above objects
         self.inventory = Inventory(loader=self.loader, variable_manager=self.variable_manager, host_list=hosts_file)
         self.variable_manager.set_inventory(self.inventory)
+
+        # --limit is defined by the 'subset' option and inventory kwarg.
+        self.host_limit = self._set_inventory_limit()
+
+    def _set_inventory_limit(self):
         hostname = None
-        if self.options.subset:
-            hostname = self.options.subset['ip']  # ['hostname']
-            self.inventory.subset(hostname)
-
-        # Playbook to run. Assumes it is
-        # local to this python file
-        if isinstance(playbook, basestring):
-            if not os.path.exists(playbook):
-                # See if path is relative, not absolute..
-                pb_dir = os.path.dirname(__file__)
-                playbook = "%s/%s" % (pb_dir, playbook)
-            if os.path.isdir(playbook):
-                playbooks = list_playbooks(playbook)
-            else:
-                playbooks = [playbook]
-        elif isinstance(playbook, list):
-            playbooks = playbook
-
-        self.playbook = playbook
-
-        hosts = self.inventory.get_hosts()
-        self.options.logger.info("Running playbooks: %s on hosts: %s" % (playbooks, hosts))
+        if self.options.subset and 'hostname' in self.options.subset:
+            hostname = self.options.subset['hostname']
+        elif self.options.subset and 'ip' in self.options.subset:
+            hostname = self.options.subset['ip']
         if hostname:
-            variables = self.inventory.get_vars(hostname)
-            group_names = variables.get('group_names',[])
-            for group_name in group_names:
-                file_path = group_vars_map.get(group_name,'')
-                if os.path.exists(file_path):
-                    self.variable_manager.add_group_vars_file(file_path,self.loader)
-            self.options.logger "Vars found: %s" % variables
+            self.inventory.subset(hostname)
+        return hostname
 
-        # Setup playbook executor, but don't run until run() called
-        self.pbex = playbook_executor.PlaybookExecutor(
-            playbooks=playbooks,
-            inventory=self.inventory,
-            variable_manager=self.variable_manager,
-            loader=self.loader,
-            options=self.options,
-            passwords=None)
+    def _set_playbooks(self, playbook):
+        # Set playbooks as list or string
+        if isinstance(playbook, basestring):
+            self._set_playbooks_from_path(playbook)
+        elif isinstance(playbook, list):
+           self.playbooks = playbook
+        else:
+           raise TypeError("Expected 'playbook' as list or string, received %s" % type(playbook))
+
+    def _set_hosts(self, group_vars_map):
+        hosts = self.inventory.get_hosts()
+        self.options.logger.info("Running playbooks: %s on hosts: %s" % (self.playbooks, hosts))
+        if self.host_limit:
+            self._include_group_vars(group_vars_map)
+
+    def run(self):
         self.pbex._tqm.load_callbacks()
         self.pbex._tqm.send_callback(
             'start_logging',
             logger=self.options.logger,
             username=self.run_data.get('ATMOUSERNAME',"No-User"),
         )
-
-    def run(self):
         # Results of PlaybookExecutor in stats.
         self.pbex.run()
         stats = self.pbex._tqm._stats
@@ -193,7 +232,7 @@ def _get_files(directory):
     return files
 
 
-def get_playbook_runner(playbook, **kwargs):
+def get_playbook_runner(playbook, logger, **kwargs):
     """
     Walk the Directory structure and return an ordered list of
     playbook objects.
@@ -206,8 +245,9 @@ def get_playbook_runner(playbook, **kwargs):
            * Playbooks are not run.
            * Playbook files use the .yml file extension.
     """
+
     if 'extra_vars' in kwargs and 'run_data' not in kwargs:
-        self.options.logger.warn(
+        logger.warn(
             "WARNING: Use 'run_data' to pass extra_vars to the playbook runner"
         )
         run_data = kwargs['extra_vars']
@@ -215,7 +255,6 @@ def get_playbook_runner(playbook, **kwargs):
         run_data = kwargs.get('run_data', {})
 
     host_file = kwargs.get('host_list')
-    logger = kwargs.get('logger')
     group_vars_map = kwargs.get('group_vars_map')
     private_key_file = kwargs.get('private_key', 'No Private Key provided')
     runner = Runner(
@@ -230,7 +269,7 @@ def get_playbook_runner(playbook, **kwargs):
     return runner
 
 
-def list_playbooks(directory, limit=[]):
+def get_playbook_files(playbook_dir, limit=[]):
     """
     Walk the Directory structure and return an ordered list of
     playbook objects.
@@ -243,4 +282,4 @@ def list_playbooks(directory, limit=[]):
            * Playbooks are not run.
            * Playbook files use the .yml file extension.
     """
-    return [pb for pb in _get_files(directory) if not limit or pb.split('/')[-1] in limit]
+    return [pb for pb in _get_files(playbook_dir) if not limit or pb.split('/')[-1] in limit]

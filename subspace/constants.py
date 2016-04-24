f8c68831828f9a7d81923effa41fe1ca683de4c4
Steve Gregory <contact@steve-gregory.com>
2016-04-24 13:20:51 -0700

    Update constants, add 'force_reload()'

--- subspace/constants.py
+++ subspace/constants.py
@@ -1,12 +1,17 @@
-import sys
 import ansible
 from ansible import constants as ansible_constants
 
 
-__all__ = ["set", "get", "reload_config"]
+__all__ = ["set_value", "get", "reload_config", "force_reload"]
+
 
 def reload_config():
     config_parser, file_loc = ansible_constants.load_config_file()
+    force_reload()  # New config file takes affect after force_reload
+    return config_parser, file_loc
+
+
+def force_reload():
     reload(ansible_constants)
     reload(ansible.plugins)
     reload(ansible.executor.task_queue_manager)

17add54965fe7d9b4fbd4212187b08880f957a29
Steve Gregory <contact@steve-gregory.com>
2016-05-09 21:04:39 -0700

    Version 2.2 - Includes CacheBusting and Improved Logging/stats

--- subspace/version.py
+++ subspace/version.py
@@ -5,7 +5,7 @@ from subprocess import Popen, PIPE
 from os.path import abspath, dirname
 
 
-VERSION = (0, 2, 1, 'dev', 0)
+VERSION = (0, 2, 2, 'dev', 0)
 
 
 git_match = "(?P<git_flag>git://)\S*#egg="\
